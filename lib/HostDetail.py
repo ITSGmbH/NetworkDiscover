80b44ad7fd64ca29652be03ccd21d6a519fcd009
Lukas Zurschmiede <lukas@ranta.ch>
2021-11-24 18:10:38 +0100

    adding OS in host table

--- lib/HostDetail.py
+++ lib/HostDetail.py
@@ -4,6 +4,8 @@ class HostDetail(xml.sax.ContentHandler):
 	def __init__(self):
 		self.current = {}
 		self.hosts = []
+		self.currentOs = None
+		self.updated_hosts = []
 		self.tmp = tempfile.TemporaryDirectory()
 
 	def __del__(self):
@@ -29,12 +31,15 @@ class HostDetail(xml.sax.ContentHandler):
 		elif tag == 'service':
 			self.current['service'] = attrs.get('name')
 			self.current['product'] = attrs.get('product')
+		elif tag == 'osmatch' and self.currentOs == None:
+			self.currentOs = attrs.get('name') + ' (' + attrs.get('accuracy', '0') + '%)'
 
 	def enrich(self, hosts = []):
 		for host in hosts:
 			self.performPortScan(host)
 
 	def performPortScan(self, target):
+		self.currentOs = None
 		out = self.tmp.name + '/details_%s.xml' % target
 		return_value = subprocess.call(['nmap', '-O', '-sT', '-sV', '-sC', '-oX', out, target], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
@@ -55,6 +60,10 @@ class HostDetail(xml.sax.ContentHandler):
 
 			host_id = storage.get('hosts', [ 'rowid' ], [ ('ip=?', ip, 'AND') ])[0][0]
 			h['host'] = host_id
+
+			if host_id not in self.updated_hosts:
+				self.updated_hosts.append(host_id)
+				storage.update('hosts', { 'os': self.currentOs, 'last_seen': 'CURRENT_TIMESTAMP' }, [ ('rowid=?', host_id) ])
 
 			existing = storage.get('ports', [ 'rowid' ], [ ('host=?', host_id, 'AND'), ('port=?', host.get('port'), 'AND'), ('protocol=?', host.get('protocol'), 'AND') ])
 			if len(existing) == 0:
