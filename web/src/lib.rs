4625aa9b220793b39085a0058152f8a645e8c913
Lukas Zurschmiede <lukyluke_ch@ranta.ch>
2022-05-25 17:05:47 +0200

    Static files and empty api services

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,9 +1,17 @@
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use std::sync::Mutex;
+use actix_web::{get, post, web, App, HttpServer, Result, Responder};
+use actix_files::{Files, NamedFile};
+use std::{path::PathBuf, sync::Mutex};
+use serde::Serialize;
 
 use std::thread;
 use network::scan;
+
+#[derive(Serialize)]
+struct StateResponse {
+	network: String,
+	state: String,
+}
 
 pub async fn run(config: config::AppConfig) -> std::io::Result<()> {
 	let running = web::Data::new(Mutex::new(false));
@@ -12,7 +20,7 @@ pub async fn run(config: config::AppConfig) -> std::io::Result<()> {
 	HttpServer::new(move || App::new()
 		.app_data(web_conf.clone())
 		.app_data(running.clone())
-		.service(actix_files::Files::new("/static", "."))
+		.service(Files::new("/static", "./static").show_files_listing())
 		.service(index)
 		.service(scan_start)
 	)
@@ -26,22 +34,56 @@ pub async fn run(config: config::AppConfig) -> std::io::Result<()> {
 }
 
 #[get("/")]
-async fn index(config: web::Data<config::AppConfig>) -> String {
-	let name = &config.name;
-	format!("Welcome to {}", name)
+async fn index() -> Result<NamedFile> {
+	let path: PathBuf = "./static/index.html".parse().unwrap();
+	Ok(NamedFile::open(path)?)
 }
 
-#[get("/scan/start")]
-async fn scan_start(config: web::Data<config::AppConfig>, running: web::Data<Mutex<bool>>) -> HttpResponse {
+#[get("/api/networks")]
+async fn show_networks(config: web::Data<config::AppConfig>) -> Result<impl Responder> {
+	Ok(web::Json(""))
+}
+
+#[get("/api/status")]
+async fn show_status(config: web::Data<config::AppConfig>, running: web::Data<Mutex<bool>>) -> Result<impl Responder> {
+	Ok(web::Json(""))
+}
+
+#[get("/api/scans")]
+async fn show_scans(config: web::Data<config::AppConfig>) -> Result<impl Responder> {
+	Ok(web::Json(""))
+}
+
+#[get("/api/network")]
+async fn get_network(config: web::Data<config::AppConfig>) -> Result<impl Responder> {
+	Ok(web::Json(""))
+}
+
+#[get("/api/info")]
+async fn get_info(config: web::Data<config::AppConfig>) -> Result<impl Responder> {
+	Ok(web::Json(""))
+}
+
+#[get("/api/scan_now")]
+async fn scan_start(config: web::Data<config::AppConfig>, running: web::Data<Mutex<bool>>) -> Result<impl Responder> {
 	let is_running = *running.lock().unwrap();
+	let mut message = "running";
 	if !is_running {
+		let conf = config.clone();
 		thread::spawn(move|| {
 			*running.lock().unwrap() = true;
-			let local = local_net::discover(&config.device);
-			scan::full(&config.clone(), &local);
+			let local = local_net::discover(&conf.device);
+			scan::full(&conf, &local);
 			*running.lock().unwrap() = false;
 		});
-		return HttpResponse::Ok().body("started");
+		message = "started";
 	}
-	HttpResponse::Ok().body("running")
+
+	let response = StateResponse {
+		network: String::from(&config.name),
+		state: String::from(message),
+	};
+	Ok(web::Json(response))
 }
+
+
