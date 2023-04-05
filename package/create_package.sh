8d09a9e4efde2bd80903bd91ab14820fed21abd2
Lukas Zurschmiede <lukyluke_ch@ranta.ch>
2023-04-05 15:57:30 +0200

    package also the readme and license

--- package/create_package.sh
+++ package/create_package.sh
@@ -16,6 +16,8 @@ mkdir -p pkg/etc/systemd/system/ &>/dev/null
 cp network_discover.service pkg/etc/systemd/system/ &>/dev/null
 cp ../target/release/network_discover pkg/opt/network_discover/ &>/dev/null
 cp -R ../static pkg/opt/network_discover/static &>/dev/null
+cp ../LICENSE pkg/opt/network_discover/ &>/dev/null
+cp ../README.md pkg/opt/network_discover/ &>/dev/null
 
 chmod +x pkg/opt/network_discover/network_discover &>/dev/null
 chmod +x pkg/etc/systemd/system/network_discover.service &>/dev/null
